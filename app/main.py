cffee4ecc25bf54a7b27f39cdfff1c01b2ccab2c
Aayush786-21 <aayushadhikari786@gmail.com>
2025-07-02 00:04:10 +0545

    update requirements

--- app/main.py
+++ app/main.py
@@ -1,15 +1,13 @@
-from flask import Flask
+# main.py
+from fastapi import FastAPI
+import uvicorn
 
-# Create the Flask application
-app = Flask(__name__)
+app = FastAPI()
 
-# Define a single route for the homepage
-@app.route('/')
-def hello_world():
-    return '<h1>Hello from your AI-Deployed Application!</h1><p>The DevOps Butler did its job.</p>'
+@app.get("/")
+def root():
+    return {"message": "Server running"}
 
-# This is the most important part for Docker
-if __name__ == '__main__':
-    # This starts a web server that runs forever
-    # and listens on all network interfaces (0.0.0.0) inside the container.
-    app.run(host='0.0.0.0', port=8000)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+
